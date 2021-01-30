7e57d31f743bab3c0b66474f86da6069f1ddad7a
FlexodBR <72344437+FlexodBR@users.noreply.github.com>
2021-01-30 19:25:49 -0300

    Update version

--- lua_samp/LuaMain.h
+++ lua_samp/LuaMain.h
@@ -9,7 +9,7 @@
 
 #define PLUGIN_MAJOR			"0"
 #define PLUGIN_MINOR			"2"
-#define PLUGIN_REVISION			"5"
-#define PLUGIN_VERSION			"v0.2.5"
+#define PLUGIN_REVISION			"6"
+#define PLUGIN_VERSION			"v0.2.6"
 
 #endif
